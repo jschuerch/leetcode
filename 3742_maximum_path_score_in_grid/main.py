3763ebbed965c32db25f2b57f58ce1ebbfd8900a
Jennifer Schürch <jennifermschuerch@gmail.com>
2026-04-30 18:03:29 +0700

    Add top-down version

--- 3742_maximum_path_score_in_grid/main.py
+++ 3742_maximum_path_score_in_grid/main.py
@@ -1,7 +1,32 @@
 from typing import List
+from functools import lru_cache
 
 class Solution:
+    
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
+        @lru_cache(maxsize=None)
+        def dp(i: int, j: int, c: int) -> float:
+            # base case: if we are at the top left corner, return 0
+            if (i == 0 and j == 0): 
+                return 0
+            
+            if  i < 0 or j < 0:
+                return float('-inf')
+
+            score = grid[i][j]
+            cost = 1 if score > 0 else 0
+
+            # this path costs too much, return -1
+            if c - cost < 0:
+                return float('-inf')
+
+            return score + max(dp(i, j-1, c - cost), dp(i-1, j, c - cost))
+
+        max_score = dp(len(grid)-1, len(grid[0])-1, k)
+
+        return -1 if max_score < 0 else int(max_score)
+
+    def maxPathScore_bottom_up(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
 
         dp = [[[-1]*(k+1) for _ in range(n)] for _ in range(m)]
@@ -33,9 +58,10 @@ if __name__ == "__main__":
     sol = Solution()
     test_cases = [
             ([[0, 1],[2, 0]], 1, 2),
-            ([[0, 1],[1, 2]], 1, -1)
+            ([[0, 1],[1, 2]], 1, -1),
+            ([[0],[2],[1],[0],[1]], 2, -1)
         ]
     for grid, k, expected in test_cases:
         result = sol.maxPathScore(grid, k)
         print(f"Input: grid={grid}, k={k}, Expected: {expected}, Got: {result}")
-        #assert result == expected, f"Test failed for input grid={grid}, k={k}: expected {expected}, got {result}"
+        assert result == expected, f"Test failed for input grid={grid}, k={k}: expected {expected}, got {result}"
