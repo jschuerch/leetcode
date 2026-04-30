7eeb3946c3ed2e678a889a0efc14f53b357ce617
Jennifer Schürch <jennifermschuerch@gmail.com>
2026-04-30 14:28:22 +0700

    remove deleting items from input list by introducing a separate result variable

--- 0056_merge_intervals/main.py
+++ 0056_merge_intervals/main.py
@@ -1,16 +1,37 @@
 from typing import List
 
 class Solution:
-    # Sort intervals and merge neighboring intervals if they overlap
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if len(intervals) == 1:
+            return intervals
+        result = []
         intervals.sort()
         i = 0
-        while i < len(intervals)-1:
-            if intervals[i][1] >= intervals[i+1][0]:
-                if intervals[i][1] < intervals[i+1][1]:
-                    intervals[i][1] = intervals[i+1][1]
-                del intervals[i+1]
+        j = 1
+        while i < len(intervals)-1 and j < len(intervals):
+            if intervals[i][1] >= intervals[j][0]:
+                if intervals[i][1] < intervals[j][1]:
+                    intervals[i][1] = intervals[j][1]
+                if j == len(intervals)-1:
+                    result.append(intervals[i])
+                j += 1
             else:
-                i += 1
-        return intervals
+                # interval i is now separate from the intervals j and later
+                result.append(intervals[i])
+                if j == len(intervals)-1:
+                    result.append(intervals[j])
+                i = j
+                j += 1
+        return result
 
+if __name__ == "__main__": 
+    sol = Solution()
+    test_cases = [
+            ([[1,3],[2,6],[8,10],[15,18]], [[1,6],[8,10],[15,18]]),
+            ([[1,4],[4,5]], [[1,5]]),
+            ([[4,7],[1,4]], [[1,7]])
+        ]
+    for x, expected in test_cases:
+        result = sol.merge(x)
+        print(f"Input: {x}, Expected: {expected}, Got: {result}")
+        assert result == expected, f"Test failed for input {x}: expected {expected}, got {result}"
