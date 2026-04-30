c74c3e3efc6bbe631df4c9a4a7d63cb00d9c570e
Jennifer Schürch <jennifermschuerch@gmail.com>
2026-04-30 17:05:36 +0700

    remove dictionary to optimize

--- 3742_maximum_path_score_in_grid/main.py
+++ 3742_maximum_path_score_in_grid/main.py
@@ -1,33 +1,31 @@
-import numpy as np
 from typing import List
 
 class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        dp = np.empty((m, n), dtype=object)
+        m, n = len(grid), len(grid[0])
+
+        dp = [[[-1]*(k+1) for _ in range(n)] for _ in range(m)]
+        dp[0][0][0] = 0
 
         for i in range(m):
             for j in range(n):
-                if i == 0 and j == 0:
-                    dp[i][j] = {0:0} if grid[i][j] == 0 else {1:grid[i][j]}
-                else:
-                    dp[i][j] = {}
-                    # check top and left cells and update dp[i][j] accordingly
-                    for di, dj in [(-1, 0), (0, -1)]:
-                        ni, nj = i + di, j + dj
-                        if 0 <= ni < m and 0 <= nj < n:
-                            for prev_k, prev_score in dp[ni][nj].items():
-                                # add cost + 1 if non score
-                                new_k = prev_k + 1 if grid[i][j] > 0 else prev_k
-                                if new_k <= k:
-                                    # if new_k already exists, take maximum of the two scores
-                                    dp[i][j][new_k] = max(dp[i][j].get(new_k, 0), prev_score + grid[i][j])
+                for c in range(k+1):
+                    if dp[i][j][c] == -1:
+                        continue
+                        
+                    if i+1 < m:
+                        score = grid[i+1][j]
+                        cost = 1 if score > 0 else 0
+                        if c + cost <= k:
+                            dp[i+1][j][c + cost] = max(dp[i+1][j][c + cost], score + dp[i][j][c])
 
-        dp_final = dp[m-1][n-1]
-        if dp_final:
-            return max(dp_final.values())
-        return -1
+                    if j+1 < n:
+                        score = grid[i][j+1]
+                        cost = 1 if score > 0 else 0
+                        if c + cost <= k:
+                            dp[i][j+1][c + cost] = max(dp[i][j+1][c + cost], score + dp[i][j][c])
+
+        return max(dp[-1][-1])
     
 
 
